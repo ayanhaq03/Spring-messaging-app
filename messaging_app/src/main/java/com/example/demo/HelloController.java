e663d5bc35eafb35f0e3bad8d989bf0386b602f5
Mohd Ayan haque <ayanhaque904@gmail.com>
2025-02-22 10:12:49 +0530

    [Mohd Ayan Haque] Refactor . updated code for uc1

--- messaging_app/src/main/java/com/example/demo/HelloController.java
+++ messaging_app/src/main/java/com/example/demo/HelloController.java
@@ -12,21 +12,5 @@ public class HelloController {
         return "Hello from BridgeLabz";
     }
 
-    // POST method - Accepts a name and returns a personalized hello message
-    @PostMapping("/hello")
-    public String sayHelloPost(@RequestParam String name) {
-        return "Hello, " + name + " from BridgeLabz!";
-    }
-
-    // PUT method - Updates and responds with a message
-    @PutMapping("/hello")
-    public String updateHello(@RequestParam String name) {
-        return "Hello, " + name + "! Your greeting has been updated.";
-    }
-
-    // DELETE method - Deletes user and responds with a message
-    @DeleteMapping("/hello")
-    public String deleteHello(@RequestParam String name) {
-        return "Goodbye, " + name + "! Your greeting has been deleted.";
-    }
+   
 }
