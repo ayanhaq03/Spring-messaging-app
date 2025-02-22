53c1bc9b58cca16bbd7d08d9475a2b238b51a71d
Mohd Ayan haque <ayanhaque904@gmail.com>
2025-02-22 10:29:05 +0530

    [Mohd Ayan Haque ] Refactor . updated code for uc2 branch messangin app

--- messaging_app/src/main/java/com/example/demo/HelloController.java
+++ messaging_app/src/main/java/com/example/demo/HelloController.java
@@ -6,12 +6,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class HelloController {
 
-    // GET method - Returns "Hello from BridgeLabz"
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello from BridgeLabz";
-    }
    
-    
+    // Handles GET requests to /hello/query with a name parameter
+    @GetMapping("/query")
+    public String sayHelloWithName(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
    
 }
